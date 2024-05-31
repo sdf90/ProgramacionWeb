207d560a8b3bffc13abaf97d3ccef6538500d18b
sdf90 <47946001+sdf90@users.noreply.github.com>
2024-05-31 10:09:45 +0200

    Creación del Modelo, Controller y View de Marca

---
+++ ProgramacionWeb/Controllers/MarcaController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProgramacionWeb.Models;
+
+namespace ProgramacionWeb.Controllers
+{
+    public class MarcaController : Controller
+    {
+        // GET: Marca
+        public ActionResult Index()
+        {
+            //Variable de lista para asignar valores
+            List<MarcaCLS> listaMarca = null;
+            //abrir la conexión a BBDD
+            using (var bd = new BDPasajeEntities())
+            {
+                //Obtener todo lo que tenemos en la tabla MARCA
+                listaMarca = (from marca in bd.Marca
+                                             select new MarcaCLS
+                                             {
+                                                 iidmarca = marca.IIDMARCA,
+                                                 nombre = marca.NOMBRE,
+                                                 descripcion = marca.DESCRIPCION
+                                             }).ToList();
+
+
+            }
+
+
+
+            return View(listaMarca);
+        }
+    }
+}
---
+++ ProgramacionWeb/Models/MarcaCLS.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProgramacionWeb.Models
+{
+    public class MarcaCLS
+    {
+        [Display(Name = "Id Marca")]
+        public int iidmarca { get; set; }
+
+        [Display(Name = "Nombre Marca")]
+        public string nombre { get; set; }
+        [Display(Name = "Descripción Marca")]
+
+        public string descripcion { get; set; }
+
+    
+
+        public int bhabilitado { get; set; }
+
+
+
+    }
+}
